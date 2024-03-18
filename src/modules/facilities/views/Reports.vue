97b515a39a2dd034eb92aa13e33795ca3d9ca8f8
pius <piuskariuki19@gmail.com>
2024-03-18 13:18:23 +0300

    Referrals route

--- src/modules/facilities/views/Reports.vue
+++ src/modules/facilities/views/Reports.vue
@@ -24,7 +24,7 @@
     <div class="flex flex-col gap-[34px] px-[27px] py-[20px] border-[.5px] border-[#c4c4c4] bg-white shadow-md rounded-[10px]">
       <div class="flex w-full justify-between">
         <p class="">Referrals</p>
-        <router-link class="text-[#067B40]  leading-[29px] font-medium underline" to="/">View all</router-link>
+        <router-link class="text-[#067B40]  leading-[29px] font-medium underline" to="/facility/referrals">View all</router-link>
       </div>
       <EasyDataTable
           border-cell

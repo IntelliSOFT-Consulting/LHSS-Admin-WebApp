69277b657f2cd9fef9f2ca24b49c3d91f9325fa9
menyagah <menyagah27@gmail.com>
2024-04-04 14:10:37 +0300

    Update vite configuration file and change dockerfile commands

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM node:16
+FROM node
 
 WORKDIR /app
 
-COPY package*.json ./
+COPY package*.json .
 
-RUN npm install
+RUN npm i
 
 COPY . .
 
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
-import {UnpluginVueComponentsResolver} from 'maz-ui/resolvers'
+import { UnpluginVueComponentsResolver } from 'maz-ui/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        Components({
-            dts: true,
-            resolvers: [UnpluginVueComponentsResolver()],
-        }),
-    ],
+	plugins: [
+		vue(),
+		Components({
+			dts: true,
+			resolvers: [UnpluginVueComponentsResolver()]
+		})
+	],
 
+	server: {
+		watch: {
+			usePolling: true
+		},
+		host: true, // needed for the Docker Container port mapping to work
+		strictPort: true,
+		port: 5173 // you can replace this port with any port
+	}
 })

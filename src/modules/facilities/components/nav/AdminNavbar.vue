12417ab7ba6be138c4ba956488ce00bf0fa7bf63
pius <piuskariuki19@gmail.com>
2024-05-03 11:13:11 +0300

    (chore)Update the top navigation font type to match the rest of the sections. If you can think of some icons possibly you can add them too.
           Also, Update the Nave color from transparent to Opaque so That when a user scrolls the body section they don’t see the content inside the nave appearing through the color. Make the color solid

--- src/modules/facilities/components/nav/AdminNavbar.vue
+++ src/modules/facilities/components/nav/AdminNavbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-      class="flex items-center justify-between px-4 py-2 md:px-[49px] md:py-6 border-[.5px] border-[#c4c4c4] shadow-lg bg-secondary/90  text-white fixed top-0 w-full z-50">
+      class="flex items-center justify-between px-4 py-2 md:px-[49px] md:py-6 border-[.5px] border-[#c4c4c4] shadow-lg bg-secondary  text-white fixed top-0 w-full z-50">
     <div class="flex items-center w-full justify-between lg:gap-2">
       <div class="flex items-center gap-2 lg:hidden">
         <maz-icon @click="isOpen = !isOpen" name="bars-4" class="w-6 cursor-pointer "/>
@@ -14,7 +14,7 @@
 
     <div
         :class="[isOpen ? 'flex': 'hidden lg:flex']"
-        class="fixed  top-0 left-0 h-screen w-4/5 z-20 flex-col px-4 pt-4 gap-8 text-lg md:text-sm italic text-white lg:relative lg:h-full lg:flex-row lg:items-center lg:justify-end  bg-secondary lg:bg-transparent">
+        class="fixed  top-0 left-0 h-screen w-4/5 z-20 flex-col px-4 pt-4 gap-8 text-lg md:text-sm  text-white lg:relative lg:h-full lg:flex-row lg:items-center lg:justify-end  bg-secondary lg:bg-transparent">
       <maz-icon @click="isOpen = false" name="x-circle" size="2rem" class=" text-white cursor-pointer lg:hidden"/>
       <router-link
           @click="isOpen = false"

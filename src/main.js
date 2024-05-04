b70b4b51ad7948f5f76a2d1e388b26a8f3bdb990
pius <piuskariuki19@gmail.com>
2024-05-04 12:43:00 +0300

    (feat)Added chevron and user icons to library

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import 'vue3-easy-data-table/dist/style.css';
 import App from './App.vue'
 import {router} from "./shared/router/index.js";
 import {installToaster} from 'maz-ui'
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {faChevronLeft, faChevronRight, faFileExport, faUserCircle} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faChevronRight, faChevronLeft, faUserCircle, faFileExport)
 
 const app = createApp(App)
 
@@ -18,6 +23,7 @@ const toasterOptions = {
 }
 
 app
+    .component('font-awesome-icon', FontAwesomeIcon)
     .use(router)
     .use(installToaster, toasterOptions)
     .component("EasyDataTable", Vue3EasyDataTable)

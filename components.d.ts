1b416ac1e0e1134dd59bae40a176157c73fdc3c4
pius <piuskariuki19@gmail.com>
2024-05-06 13:51:25 +0300

    (chore)import optimization for maz components

--- components.d.ts
+++ components.d.ts
@@ -8,12 +8,15 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     MazBadge: typeof import('maz-ui/components/MazBadge')['default']
+    MazBtn: typeof import('maz-ui/components/MazBtn')['default']
     MazDataList: typeof import('maz-ui/components/MazDataList')['default']
     MazDataListItem: typeof import('maz-ui/components/MazDataListItem')['default']
     MazIcon: typeof import('maz-ui/components/MazIcon')['default']
     MazInput: typeof import('maz-ui/components/MazInput')['default']
+    MazPhoneNumberInput: typeof import('maz-ui/components/MazPhoneNumberInput')['default']
     MazSelect: typeof import('maz-ui/components/MazSelect')['default']
     MazSp: typeof import('maz-ui/components/MazSp')['default']
+    MazSpinner: typeof import('maz-ui/components/MazSpinner')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }

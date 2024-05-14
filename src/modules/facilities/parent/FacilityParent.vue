74f20e594cab3b46c1cef323bbd8a27c8492ef26
pius <piuskariuki19@gmail.com>
2024-05-14 11:06:13 +0300

    (refactor) Removed redundant pinia store

--- src/modules/facilities/parent/FacilityParent.vue
+++ src/modules/facilities/parent/FacilityParent.vue
@@ -1,7 +1,8 @@
 <template>
-  <div  class="flex flex-col w-screen  overflow-y-scroll relative">
-    <AdminNavbar :is-open="isOpen" />
-    <div class="flex px-4 pb-8 lg:px-16 lg:pb-10 pt-20 md:pt-40 lg:pt-40 bg-[#f5f5f5] min-h-[100vh] w-full overflow-x-clip">
+  <div class="flex flex-col w-screen  overflow-y-scroll relative">
+    <AdminNavbar :is-open="isOpen"/>
+    <div
+        class="flex px-4 pb-8 lg:px-16 lg:pb-10 pt-20 md:pt-40 lg:pt-40 bg-[#f5f5f5] min-h-[100vh] w-full overflow-x-clip">
       <router-view/>
     </div>
   </div>
@@ -9,28 +10,9 @@
 
 <script setup>
 import AdminNavbar from "../components/nav/AdminNavbar.vue";
-import {onMounted, ref} from "vue";
-import {useLocationStore} from "../../../shared/store/locationStore.js";
-import {useAxios} from "../../../shared/hooks/useAxios.js";
+import {ref} from "vue";
 
 const isOpen = ref(false)
 
-const locationStore = useLocationStore()
-
-const {makeRequest} = useAxios()
-
-
-const getAllLocations = async () => {
-  try {
-    const response = await makeRequest({url: 'Location'})
-    locationStore.setLocations(response.entry)
-  } catch (error) {
-    console.error("error", error)
-  }
-}
-
-onMounted(()=>{
-  getAllLocations()
-})
 
 </script>

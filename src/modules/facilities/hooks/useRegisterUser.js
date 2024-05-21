4a2df7ff77ae00e151c4341ce39d9a4e57d50686
pius <piuskariuki19@gmail.com>
2024-05-21 13:10:11 +0300

    (feature) register admin workflow

--- src/modules/facilities/hooks/useRegisterUser.js
+++ src/modules/facilities/hooks/useRegisterUser.js
@@ -1,17 +1,76 @@
 import {ref} from "vue";
+import {useAxios} from "../../../shared/hooks/useAxios.js";
+import {useToast} from "maz-ui";
 
 export const useRegisterUser = () => {
     const loading = ref(false);
 
-    const firstname=ref("")
-    const lastname=ref("")
-    const idNumber=ref("")
-    const email=ref("")
-    const phone=ref("")
+    const firstname = ref("")
+    const lastname = ref("")
+    const idNumber = ref("")
+    const email = ref("")
+    const phone = ref("")
+    const password = ref("")
+    const confirmPassword = ref("")
+    const showPasswordError = ref(false)
+    const isDialogOpen = ref(false)
+
+    const {makeNormalRequest} = useAxios()
+
+    const toast = useToast()
 
     const submit = async (evt) => {
         evt.preventDefault()
+        try {
+            loading.value = true;
+            if (showPasswordError.value) return
+
+            const response = await makeNormalRequest({
+                url: "/auth/provider/register",
+                method: "POST",
+                data: {
+                    "idNumber": idNumber.value,
+                    "password": password.value,
+                    "email": email.value,
+                    "firstName": firstname.value,
+                    "lastName": lastname.value,
+                    "phone": phone.value,
+                    "role": "ADMINISTRATOR"
+                }
+            })
+
+            if (response.status === "success") {
+                isDialogOpen.value = !isDialogOpen.value
+                setTimeout(() => {
+                    isDialogOpen.value = false
+                    idNumber.value = ""
+                    password.value = ""
+                    confirmPassword.value = ""
+                    email.value = ""
+                    firstname.value = ""
+                    lastname.value = ""
+                    phone.value = ""
+                }, 1000)
+            }
+
+        } catch (err) {
+            toast.error(err.response.data.error)
+        } finally {
+            loading.value = false
+        }
     }
 
-    return {submit, loading, firstname, lastname, idNumber, email, phone}
+    return {
+        submit,
+        loading,
+        firstname,
+        lastname,
+        idNumber,
+        email,
+        phone,
+        password,
+        confirmPassword,
+        showPasswordError,
+        isDialogOpen
+    }
 }
--- src/modules/facilities/views/RegisterUser.vue
+++ src/modules/facilities/views/RegisterUser.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex flex-col bg-white shadow-md rounded-[10px] border-[#c4c4c4] border-[.5px] w-full relative p-4 gap-8">
-    <p class="text-xl font-medium text-[#292929]">Add user</p>
+    <div class="flex items-center gap-2">
+      <font-awesome-icon icon="fa-solid fa-user-plus" class="w-6 h-6 text-primary"/>
+      <p class="text-xs lg:text-lg font-medium text-[#292929]">Add User</p>
+    </div>
     <form
         class="grid lg:grid-cols-3 gap-8  w-full "
         autocomplete="off"
@@ -18,6 +21,18 @@
           country-code="ET"
           :only-countries="['ET', 'DJ']"
           label="Phone Number" type="phone"/>
+
+      <div class="col-span-full border-b-2 text-primary">Security</div>
+      <maz-input v-model="password" required label="Password" type="password"/>
+      <maz-input
+          :error="showPasswordError ? true: false"
+          v-model="confirmPassword"
+          required
+          label="Confirm Password"
+          type="password"
+          :hint="showPasswordError ? 'Passwords do not match': null"/>
+
+
       <div
           class="flex items-center w-full col-span-full justify-end gap-[25px] border-t-[.5px] border-t-[#C4C4C4] py-[11px] px-5 mt-auto absolute bottom-0 left-0">
         <MazSpinner color="secondary" v-if="loading"/>
@@ -27,6 +42,18 @@
         </div>
       </div>
     </form>
+
+    <maz-dialog v-model="isDialogOpen" title="Success" class="border-[.5px]">
+      <template #header="{ close }">
+        <p class="flex bg-[#0B8648] w-full px-8 py-[15px] text-white font-medium text-[14px]">Success.</p>
+      </template>
+      <div class="flex flex-col items-center gap-8">
+        <font-awesome-icon icon="fa-solid fa-check-circle" class="w-1/2 h-1/2 text-primary"/>
+        <p class="text-center">User registered successfully!</p>
+        <maz-btn @click="close" size="lg">Close</maz-btn>
+      </div>
+    </maz-dialog>
+
   </div>
 </template>
 
@@ -34,9 +61,33 @@
 <script setup>
 import {useRegisterUser} from "../hooks/useRegisterUser.js";
 import {useRouter} from "vue-router";
+import {watch} from "vue";
+import MazDialog from "maz-ui/components/MazDialog";
+import MazBtn from "maz-ui/components/MazBtn";
 
 const router = useRouter()
 
-const {submit, loading, firstname, lastname, idNumber, email, phone} = useRegisterUser()
+const {
+  submit,
+  loading,
+  firstname,
+  lastname,
+  idNumber,
+  email,
+  phone,
+  password,
+  confirmPassword,
+  showPasswordError,
+  isDialogOpen
+} = useRegisterUser()
+
+watch([password, confirmPassword], (values) => {
+  if (values[0].length > 0 && values[1].length > 0) {
+    if (values[0] !== values[1])
+      showPasswordError.value = true
+    else
+      showPasswordError.value = false
+  }
+})
 
 </script>
